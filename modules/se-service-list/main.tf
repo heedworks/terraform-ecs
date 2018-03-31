796166761e9ed812401257d0d61ef4a703991ad6
Neal Erickson <neal@ignite.tech>
2018-03-30 20:59:32 -0400

    Break variables out into a separate file

--- modules/se-service-list/main.tf
+++ modules/se-service-list/main.tf
@@ -1,206 +1,63 @@
-variable "cluster" {
-  description = ""
-  default     = "se-app"
+# -----------------------------------------------------------------------------
+# S3 Bucket for hosting se-widget-embed
+# -----------------------------------------------------------------------------
+data "template_file" "se_widget_embed_policy" {
+  template = "${file("${path.module}/templates/public-read-policy.json")}"
+
+  vars = {
+    bucket = "se-widget-embed-${var.aws_account_key}"
+  }
 }
 
-variable "region" {
-  description = ""
-  default     = "us-east-1"
+resource "aws_s3_bucket" "se_widget_embed" {
+  bucket = "se-widget-embed-${var.aws_account_key}"
+
+  versioning {
+    enabled = true
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags {
+    Name        = "se-widget-embed-${var.aws_account_key}"
+    Environment = "${var.environment}"
+  }
+
+  policy = "${data.template_file.se_widget_embed_policy.rendered}"
 }
 
-variable "environment" {
-  description = ""
+# -----------------------------------------------------------------------------
+# S3 Bucket for hosting se-widget-ui-haller
+# -----------------------------------------------------------------------------
+data "template_file" "se_widget_ui_haller_policy" {
+  template = "${file("${path.module}/templates/public-read-policy.json")}"
+
+  vars = {
+    bucket = "se-widget-ui-haller-${var.aws_account_key}"
+  }
 }
 
-variable "default_container_port" {
-  description = "default container port"
-  default     = 8000
-}
+resource "aws_s3_bucket" "se_widget_ui_haller" {
+  bucket = "se-widget-ui-haller-${var.aws_account_key}"
 
-variable "default_image_tag" {
-  description = "The default image_tag for ECR images"
-}
+  versioning {
+    enabled = true
+  }
 
-variable "default_node_env" {
-  description = "The default NODE_ENV environment variable"
-}
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 
-variable "default_se_env" {
-  description = "The default SE_ENV environment variable"
-}
+  tags {
+    Name        = "se-widget-ui-haller-${var.aws_account_key}"
+    Environment = "${var.environment}"
+  }
 
-variable "default_task_cpu" {
-  description = "default number of cpu units to reserve for the container"
-  default     = 0
-}
-
-variable "default_task_memory" {
-  description = "default maximum number of MiB of memory to reserve for the container"
-  default     = 256
-}
-
-variable "default_task_memory_reservation" {
-  description = "default number of MiB of memory to reserve for the container"
-  default     = 64
-}
-
-variable "default_desired_count" {
-  description = "The desired task instance count"
-  default     = 2
-}
-
-variable "default_deployment_minimum_healthy_percent" {
-  description = "default lower limit (% of desired_count) of # of running tasks during a deployment"
-  default     = 100
-}
-
-variable "default_deployment_maximum_percent" {
-  description = "default upper limit (% of desired_count) of # of running tasks during a deployment"
-  default     = 200
-}
-
-variable "port_map" {
-  description = "map of service name and host port"
-  type        = "map"
-
-  default = {}
-}
-
-#------------------------------------------------------------------------------
-# Service Override Maps
-#------------------------------------------------------------------------------
-variable "container_port_map" {
-  description = "map of service name to container port to override the default_container_port variable, if applicable"
-  type        = "map"
-
-  default = {}
-}
-
-variable "image_tag_map" {
-  description = "map of service name to container port to override the default_image_tag variable, if applicable"
-  type        = "map"
-
-  default = {}
-}
-
-variable "se_env_map" {
-  description = "map of service name to container port to override the default_se_env variable, if applicable"
-  type        = "map"
-
-  default = {}
-}
-
-variable "node_env_map" {
-  description = "map of service name to container port to override the default_node_env variable, if applicable"
-  type        = "map"
-
-  default = {}
-}
-
-variable "task_cpu_map" {
-  description = "map of service name to task cpu value to override the default_task_cpu variable, if applicable"
-  type        = "map"
-
-  default = {}
-}
-
-variable "task_memory_map" {
-  description = "map of service name to task memory value to override the default_task_memory variable, if applicable"
-  type        = "map"
-
-  default = {}
-}
-
-variable "task_memory_reservation_map" {
-  description = "map of service name to task memory_reservation value to override the default_task_memory_reservation variable, if applicable"
-  type        = "map"
-
-  default = {}
-}
-
-variable "kong_db_name" {
-  description = "RDS instance name"
-  default     = "se-kong-postgres"
-}
-
-variable "kong_db_password" {
-  description = "RDS user password"
-}
-
-variable "aws_account_key" {
-  description = ""
-}
-
-variable "vpc_id" {
-  description = "The VPC id"
-}
-
-variable "zone_id" {
-  description = "The ID of the hosted zone to contain this record."
-}
-
-variable "aws_account_name" {
-  description = ""
-}
-
-variable "aws_account_id" {
-  description = ""
-}
-
-variable "ecr_domain" {
-  description = ""
-}
-
-variable "ecs_cluster_security_group_id" {
-  description = ""
-}
-
-# variable "internal_alb_security_group_id" {
-#   description = ""
-# }
-
-# variable "external_alb_security_group_id" {
-#   description = ""
-# }
-
-# variable "external_ssh_security_group_id" {}
-
-variable "internal_subnet_ids" {
-  description = "A list of internal subnet IDs"
-  type        = "list"
-}
-
-variable "external_subnet_ids" {
-  description = "A list of external subnet IDs"
-  type        = "list"
-}
-
-variable "kong_db_security_groups" {
-  description = ""
-}
-
-variable "internal_alb_arn" {
-  description = ""
-}
-
-variable "external_alb_arn" {
-  description = ""
-}
-
-variable "external_alb_target_group_arn" {
-  description = ""
-}
-
-variable "internal_alb_listener_arn" {
-  description = ""
-}
-
-variable "ecs_tasks_cloudwatch_log_group" {
-  description = ""
-}
-
-variable "mongo_connection_string_template" {
-  description = "Mongo connection string with a '%s' placeholder for the database name, e.g. mongodb://user:password@cluster0-shard-00-01-hulfh.mongodb.net:27017/%s"
+  policy = "${data.template_file.se_widget_ui_haller_policy.rendered}"
 }
 
 # -----------------------------------------------------------------------------
@@ -337,8 +194,7 @@ module "se_address_service" {
   vpc_id          = "${var.vpc_id}"
   zone_id         = "${var.zone_id}"
 
-  # image            = "${var.ecr_domain}/schedule-engine/se-address-service"
-  image     = "${var.ecr_domain}/schedule-engine/se-client-service"
+  image            = "${var.ecr_domain}/schedule-engine/se-address-service"
   image_tag = "${lookup(var.image_tag_map, "se-address-service", var.aws_account_key)}"
 
   port           = 0
@@ -1085,8 +941,7 @@ module "se_geocoding_service" {
   vpc_id          = "${var.vpc_id}"
   zone_id         = "${var.zone_id}"
 
-  # image            = "${var.ecr_domain}/schedule-engine/se-geocoding-service"
-  image     = "${var.ecr_domain}/schedule-engine/se-client-service"
+  image     = "${var.ecr_domain}/schedule-engine/se-geocoding-service"
   image_tag = "${lookup(var.image_tag_map, "se-geocoding-service", var.aws_account_key)}"
 
   port           = 0
@@ -1613,8 +1468,7 @@ module "se_web_api" {
   vpc_id          = "${var.vpc_id}"
   zone_id         = "${var.zone_id}"
 
-  # image            = "${var.ecr_domain}/schedule-engine/se-web-api"
-  image     = "${var.ecr_domain}/schedule-engine/se-client-service"
+  image            = "${var.ecr_domain}/schedule-engine/se-web-api"
   image_tag = "${lookup(var.image_tag_map, "se-web-api", var.aws_account_key)}"
 
   port           = 0
---
+++ modules/se-service-list/variables.tf
@@ -0,0 +1,204 @@
+variable "cluster" {
+  description = ""
+  default     = "se-app"
+}
+
+variable "region" {
+  description = ""
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = ""
+}
+
+variable "default_container_port" {
+  description = "default container port"
+  default     = 8000
+}
+
+variable "default_image_tag" {
+  description = "The default image_tag for ECR images"
+}
+
+variable "default_node_env" {
+  description = "The default NODE_ENV environment variable"
+}
+
+variable "default_se_env" {
+  description = "The default SE_ENV environment variable"
+}
+
+variable "default_task_cpu" {
+  description = "default number of cpu units to reserve for the container"
+  default     = 0
+}
+
+variable "default_task_memory" {
+  description = "default maximum number of MiB of memory to reserve for the container"
+  default     = 256
+}
+
+variable "default_task_memory_reservation" {
+  description = "default number of MiB of memory to reserve for the container"
+  default     = 64
+}
+
+variable "default_desired_count" {
+  description = "The desired task instance count"
+  default     = 2
+}
+
+variable "default_deployment_minimum_healthy_percent" {
+  description = "default lower limit (% of desired_count) of # of running tasks during a deployment"
+  default     = 100
+}
+
+variable "default_deployment_maximum_percent" {
+  description = "default upper limit (% of desired_count) of # of running tasks during a deployment"
+  default     = 200
+}
+
+variable "port_map" {
+  description = "map of service name and host port"
+  type        = "map"
+
+  default = {}
+}
+
+#------------------------------------------------------------------------------
+# Service Override Maps
+#------------------------------------------------------------------------------
+variable "container_port_map" {
+  description = "map of service name to container port to override the default_container_port variable, if applicable"
+  type        = "map"
+
+  default = {}
+}
+
+variable "image_tag_map" {
+  description = "map of service name to container port to override the default_image_tag variable, if applicable"
+  type        = "map"
+
+  default = {}
+}
+
+variable "se_env_map" {
+  description = "map of service name to container port to override the default_se_env variable, if applicable"
+  type        = "map"
+
+  default = {}
+}
+
+variable "node_env_map" {
+  description = "map of service name to container port to override the default_node_env variable, if applicable"
+  type        = "map"
+
+  default = {}
+}
+
+variable "task_cpu_map" {
+  description = "map of service name to task cpu value to override the default_task_cpu variable, if applicable"
+  type        = "map"
+
+  default = {}
+}
+
+variable "task_memory_map" {
+  description = "map of service name to task memory value to override the default_task_memory variable, if applicable"
+  type        = "map"
+
+  default = {}
+}
+
+variable "task_memory_reservation_map" {
+  description = "map of service name to task memory_reservation value to override the default_task_memory_reservation variable, if applicable"
+  type        = "map"
+
+  default = {}
+}
+
+variable "kong_db_name" {
+  description = "RDS instance name"
+  default     = "se-kong-postgres"
+}
+
+variable "kong_db_password" {
+  description = "RDS user password"
+}
+
+variable "aws_account_key" {
+  description = ""
+}
+
+variable "vpc_id" {
+  description = "The VPC id"
+}
+
+variable "zone_id" {
+  description = "The ID of the hosted zone to contain this record."
+}
+
+variable "aws_account_name" {
+  description = ""
+}
+
+variable "aws_account_id" {
+  description = ""
+}
+
+variable "ecr_domain" {
+  description = ""
+}
+
+variable "ecs_cluster_security_group_id" {
+  description = ""
+}
+
+# variable "internal_alb_security_group_id" {
+#   description = ""
+# }
+
+# variable "external_alb_security_group_id" {
+#   description = ""
+# }
+
+# variable "external_ssh_security_group_id" {}
+
+variable "internal_subnet_ids" {
+  description = "A list of internal subnet IDs"
+  type        = "list"
+}
+
+variable "external_subnet_ids" {
+  description = "A list of external subnet IDs"
+  type        = "list"
+}
+
+variable "kong_db_security_groups" {
+  description = ""
+}
+
+variable "internal_alb_arn" {
+  description = ""
+}
+
+variable "external_alb_arn" {
+  description = ""
+}
+
+variable "external_alb_target_group_arn" {
+  description = ""
+}
+
+variable "internal_alb_listener_arn" {
+  description = ""
+}
+
+variable "ecs_tasks_cloudwatch_log_group" {
+  description = ""
+}
+
+variable "mongo_connection_string_template" {
+  description = "Mongo connection string with a '%s' placeholder for the database name, e.g. mongodb://user:password@cluster0-shard-00-01-hulfh.mongodb.net:27017/%s"
+}
